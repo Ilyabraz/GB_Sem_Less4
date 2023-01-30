c6681087a0144da0ad023cd058e2915a13be3df5
Ilya_Braz <ilya262braz@yandex.ru>
2023-01-30 22:26:37 +0300

    first commit

---
+++ Lesson4_Task27/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу, которая принимает на вход число и выдаёт сумму цифр в числе
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+            int s = 0;
+            while (a > 0)
+            {
+ 
+                s = s + a % 10;
+                a = a /10 ;
+ 
+            }
+ Console.WriteLine(s);
+
---
+++ Lesson4_Task29/Program.cs
@@ -0,0 +1,4 @@
+﻿// Написать программу, которая задаёт массив из 8 элементов и выводит их на экран
+int[] a = Enumerable.Range(1, 8).Select(x => 17 % x).ToArray(); 
+foreach (int i in a)
+  Console.WriteLine(i);
